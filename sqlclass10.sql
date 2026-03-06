a6d52919e9459357e04cf9c09a2c79e134a820cb
KARTHIK H <hkarthik15886@gmail.com>
2026-03-06 15:35:04 +0530

    sql101

--- sqlclass10.sql
+++ sqlclass10.sql
@@ -1,12 +1,13 @@
 USE mysqllabs;
-CREATE TABLE students5 (`student_id` INT(3) NOT NULL , `name` VARCHAR(50) NOT NULL , `email` VARCHAR(100) NOT NULL,UNIQUE (student_id,email));
+CREATE TABLE students5 (`student_id` INT(3) NOT NULL , `name` VARCHAR(50) NOT NULL , `email` VARCHAR(100) NOT NULL,UNIQUE (student_id,email),PRIMARY KEY (student_id));
 SELECT * FROM students5;
 
-CREATE TABLE courses5 (`course_id` INT(3) NOT NULL , `course_name` VARCHAR(50) NOT NULL ,UNIQUE (course_id));
+CREATE TABLE courses5 (`course_id` INT(3) NOT NULL , `course_name` VARCHAR(50) NOT NULL ,UNIQUE (course_id),PRIMARY KEY (course_id));
 SELECT * FROM courses5;
 
 CREATE TABLE enrollments5 (`student_id` INT(3) NOT NULL , `course_id` INT(3) NOT NULL,
 FOREIGN KEY (student_id) REFERENCES students5(student_id),
 FOREIGN KEY (course_id) REFERENCES courses5(course_id));
 SELECT * FROM enrollments5;
+ 
 
