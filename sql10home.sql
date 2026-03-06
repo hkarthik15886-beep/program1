d6582b836b739eb7db55ce4c91dce6d8a3616f7d
KARTHIK H <hkarthik15886@gmail.com>
2026-03-06 15:50:37 +0530

    sql10

--- sql10home.sql
+++ sql10home.sql
@@ -2,4 +2,4 @@ USE mysqllabs;
  CREATE TABLE authors (`author_id` INT(3) NOT NULL , `author_name` VARCHAR(50) NOT NULL,`email_address` VARCHAR(100) NOT NULL,UNIQUE (author_id,email_address),PRIMARY KEY (author_id));
 SELECT * FROM authors;
 CREATE TABLE books10 (`book_id` INT(3) NOT NULL , `book_title` VARCHAR(100) NOT NULL , `author_id` INT(4) NOT NULL, UNIQUE (book_id),FOREIGN KEY (author_id) REFERENCES authors(author_id));
-SELECT * FROM books;
+SELECT * FROM books10;
